09d031c3d2446799c367ba5dba1fde2bdcb33ea9
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2023-01-13 14:50:53 +0530

    Fix modal height issue

---
+++ src/components/ChooseTemplates.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const chooseTemplates = (props: Props) => {
+    return <h1>Create your resume. Let's start with selecting a template</h1>;
+};
+
+export default chooseTemplates;
--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -1,10 +1,15 @@
-import React from "react";
 import Navbar from "../components/Navbar";
+import ChooseTemplates from "../components/ChooseTemplates";
 
 type Props = {};
 
 const Templates = (props: Props) => {
-    return <Navbar />;
+    return (
+        <>
+            <Navbar />
+            <ChooseTemplates />
+        </>
+    );
 };
 
 export default Templates;
