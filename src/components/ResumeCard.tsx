f460bb3210a97e423f1c704088b3067948250360
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2023-01-14 07:07:45 +0530

    Update resume templates with preview and create button

--- src/components/ResumeCard.tsx
+++ src/components/ResumeCard.tsx
@@ -12,7 +12,19 @@ const Card: React.FC<Props> = ({ image, header }) => {
     return (
         <div className={styles.cardContainer}>
             <h2 className={styles.cardHeader}>{header}</h2>
-            <img src={image} alt="Card Image" className={styles.cardImage} />
+            <div className={styles.cardBody}>
+                <div className={styles.overlay}>
+                    <img
+                        src={image}
+                        alt="Card Image"
+                        className={styles.cardImage}
+                    />
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.button}>Preview</button>
+                        <button className={styles.button}>Create</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
